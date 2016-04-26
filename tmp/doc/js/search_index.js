758517fb57d1fe09f53b17b648f3324b0a22ccf6
Michael Timberg <timberg.1@osu.edu>
2016-04-26 16:11:00 -0400

    Third Commit
    Made sure migrations were run and database was seeded with test data.
    Log reset.

---
+++ tmp/doc/js/search_index.js
@@ -0,0 +1,1 @@
+var search_data = {"index":{"searchIndex":["readme"],"longSearchIndex":[""],"info":[["README","","README_rdoc.html","","<p>README\n<p>In order to get the application ruinning, you&#39;ll need:\n\n<pre>- Ruby version 2.1.5 or greater\n - To ...</pre>\n"]]}}
